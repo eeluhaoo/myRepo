381a6474e026a90a50ba846d4223689d97e35063
luhao <eeluhaoo@189.cn>
2018-08-03 09:34:02 +0800

    modified hello.php

--- hello.php
+++ hello.php
@@ -3,3 +3,6 @@ echo '菜鸟教程：www.runoob.com';
 ?>
 <p  v-if:show> show me if show is true</p>
 <button v-mode='the mode' v-on:click='myClick'>click</button>
+<div id='app-1>
+  <p class='class:ok'>hello Vue</p>
+</div>
